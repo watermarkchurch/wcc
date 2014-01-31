1e1bcaac5162a53e2a2f9a20d73f9ba94efc0aab
Travis Petticrew <travis@petticrew.net>
2014-01-31 13:41:19 -0600

    Run rake tasks in alphabetical order

--- lib/wcc/rails/railties.rb
+++ lib/wcc/rails/railties.rb
@@ -1,7 +1,7 @@
 class WCC::Railties < Rails::Railtie
 
   rake_tasks do
-    Dir[File.join(File.dirname(__FILE__), "..", "..", "tasks", "*.rake")].each do |rake_file|
+    Dir[File.join(File.dirname(__FILE__), "..", "..", "tasks", "*.rake")].sort.each do |rake_file|
       load rake_file
     end
   end
